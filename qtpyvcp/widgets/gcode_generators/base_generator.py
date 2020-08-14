03b13c049819537cacd4b545f1218435ce199fa7
Aaron Dargel <adargel@live.com>
2020-08-14 10:33:29 -0500

    WIP: Add QtProperties for all attributes

--- qtpyvcp/widgets/gcode_generators/base_generator.py
+++ qtpyvcp/widgets/gcode_generators/base_generator.py
@@ -1,40 +1,105 @@
+from qtpy.QtCore import Property
 
 class BaseGenerator(object):
     def __init__(self):
-        self.wcs = ''
-        self.speed = 0.
-        self.feed = 0.
-        self.z_feed = 0.
-        self.z_clear = 0.
-        self.tool = 0.
-        self.coolant = ''
-        self.unit = 'G20'
+        self.wcs_ = ''
+        self.speed_ = 0.
+        self.feed_ = 0.
+        self.z_feed_ = 0.
+        self.z_clear_ = 0.
+        self.tool_ = 0
+        self.coolant_ = ''
+        self.unit_ = 'G20'
+
+    @Property(str)
+    def wcs(self):
+        return self.wcs_
+
+    @wcs.setter
+    def wcs(self, value):
+        self.wcs_ = value
+
+    @Property(float)
+    def speed(self):
+        return self.speed_
+
+    @speed.setter
+    def speed(self, value):
+        self.speed_ = value
+
+    @Property(float)
+    def feed(self):
+        return self.feed_
+
+    @feed.setter
+    def feed(self, value):
+        self.speed_ = value
+
+    @Property(float)
+    def z_feed(self):
+        return self.z_feed_
+
+    @z_feed.setter
+    def z_feed(self, value):
+        self.z_feed_ = value
+
+    @Property(float)
+    def z_clear(self):
+        return self.z_clear_
+
+    @z_clear.setter
+    def z_clear(self, value):
+        self.z_clear_ = value
+
+    @Property(int)
+    def tool(self):
+        return self.tool_
+
+    @tool.setter
+    def tool(self, value):
+        self.tool_ = value
+
+    @Property(str)
+    def coolant(self):
+        return self.coolant_
+
+    @coolant.setter
+    def coolant(self, value):
+        self.coolant_ = value
+
+    @Property(str)
+    def unit(self):
+        return self.unit_
+
+    @unit.setter
+    def unit(self, value):
+        self.unit_ = value
 
     def preamble(self):
         preamble = ['G90 G94 G17 G91.1\n',
-                    '%s\n' % self.unit,
+                    '%s\n' % self.unit_,
                     'G53 G0 Z0\n',
-                    'T%i M6 G43\n' % self.tool,
-                    'S%.4f\n' % abs(self.speed)]
+                    'T%i M6 G43\n' % self.tool_,
+                    'S%.4f\n' % abs(self.speed_)]
 
-        if self.coolant:
-            preamble.append(self.coolant + '\n')
+        if self.coolant_:
+            preamble.append(self.coolant_ + '\n')
 
-        if self.speed < 0:
+        if self.speed_ < 0:
             preamble.append("M4\n")
         else:
             preamble.append("M3\n")
 
 
-        preamble.append('%s\n' % self.wcs)
-        preamble.append('F%.4f\n' % self.feed)
-        preamble.append('G0 Z%.4f\n' % self.z_clear)
+        preamble.append('%s\n' % self.wcs_)
+        preamble.append('F%.4f\n' % self.feed_)
+        preamble.append('G0 Z%.4f\n' % self.z_clear_)
 
         return preamble
 
     def epilog(self):
         output = []
-        if self.coolant:
+        if self.coolant_:
             output.append('M9\n')
         output.append('G53 G0 Z0\nM30\n%')
         return output
--- qtpyvcp/widgets/gcode_generators/drill.py
+++ qtpyvcp/widgets/gcode_generators/drill.py
@@ -1,5 +1,6 @@
 from __future__ import division
 from base_generator import BaseGenerator
+from qtpy.QtCore import Property
 
 import math
 
@@ -7,13 +8,37 @@ import math
 class Drill(BaseGenerator):
     def __init__(self):
         super(Drill, self).__init__()
-        self.z_start = 0.
-        self.z_end = 0.
-        self.hole_locations = []
+        self.z_start_ = 0.
+        self.z_end_ = 0.
+        self.hole_locations_ = []
+
+    @Property(float)
+    def z_start(self):
+        return self.z_start_
+
+    @z_start.setter
+    def z_start(self, value):
+        self.z_start_ = value
+
+    @Property(float)
+    def z_end(self):
+        return self.z_end_
+
+    @z_end.setter
+    def z_end(self, value):
+        self.z_end_ = value
+
+    @Property(list)
+    def hole_locations(self):
+        return self.hole_locations_
+
+    @hole_locations.setter
+    def hole_locations(self, value):
+        self.hole_locations_ = value
 
     def drill(self):
         output = self.preamble()
-        output.append('G98 G81 R%.4f Z%.4f F%.4f ' % (self.z_start,  self.z_end, self.z_feed))
+        output.append('G98 G81 R%.4f Z%.4f F%.4f ' % (self.z_start_,  self.z_end_, self.z_feed_))
         output.extend(self.add_holes_())
         output.extend(self.epilog())
 
@@ -21,31 +46,29 @@ class Drill(BaseGenerator):
 
     def spot(self):
         output = self.preamble()
-        output.append('G98 G81 R%.4f Z%.4f F%.4f ' % (self.z_start, self.z_end, self.z_feed))
+        output.append('G98 G81 R%.4f Z%.4f F%.4f ' % (self.z_start_, self.z_end_, self.z_feed_))
         output.extend(self.add_holes_())
         output.extend(self.epilog())
 
         return ''.join(output)
 
     def tap(self, pitch):
-        self.z_feed = self.speed * abs(pitch)
+        self.z_feed = self.speed_ * abs(pitch)
 
-        print pitch
         if pitch < 0:
-            self.speed = -self.speed
+            self.speed = -self.speed_
 
         output = self.preamble()
-        output.append('G99 G%i R%.4f Z%.4f F%.4f S%.4f ' %
-                      (74 if pitch < 0. else 84, self.z_start,  self.z_end, self.z_feed, abs(self.speed)))
+        output.append('G98 G%i R%.4f Z%.4f F%.4f S%.4f ' %
+                      (74 if pitch < 0. else 84, self.z_start_,  self.z_end_, self.z_feed_, abs(self.speed_)))
         output.extend(self.add_holes_())
         output.extend(self.epilog())
 
-        print output
         return ''.join(output)
 
     def dwell(self, dwell_time):
         output = self.preamble()
-        output.append('G98 G82 R%.4f Z%.4f P%.4f F%.4f ' % (self.z_start,  self.z_end, dwell_time, self.z_feed))
+        output.append('G98 G82 R%.4f Z%.4f P%.4f F%.4f ' % (self.z_start_,  self.z_end_, dwell_time, self.z_feed_))
         output.extend(self.add_holes_())
         output.extend(self.epilog())
 
@@ -53,7 +76,7 @@ class Drill(BaseGenerator):
 
     def peck(self, delta):
         output = self.preamble()
-        output.append('G98 G83 R%.4f Z%.4f Q%.4f F%.4f ' % (self.z_start,  self.z_end, delta, self.z_feed))
+        output.append('G98 G83 R%.4f Z%.4f Q%.4f F%.4f ' % (self.z_start_,  self.z_end_, delta, self.z_feed_))
         output.extend(self.add_holes_())
         output.extend(self.epilog())
 
@@ -61,7 +84,7 @@ class Drill(BaseGenerator):
 
     def chip_break(self, delta):
         output = self.preamble()
-        output.append('G98 G73 R%.4f Z%.4f Q%.4f F%.4f ' % (self.z_start,  self.z_end, delta, self.z_feed))
+        output.append('G98 G73 R%.4f Z%.4f Q%.4f F%.4f ' % (self.z_start_,  self.z_end_, delta, self.z_feed_))
         output.extend(self.add_holes_())
         output.extend(self.epilog())
 
@@ -77,16 +100,15 @@ class Drill(BaseGenerator):
             x += circle_center[0]
             y += circle_center[1]
             curr_angle += angle_step
-            self.hole_locations.append((x, y))
+            self.hole_locations_.append((x, y))
 
     def add_holes_(self):
         output = []
-        for hole in self.hole_locations:
+        for hole in self.hole_locations_:
             output.append('X%.4f Y%.4f\n' % (hole[0], hole[1]))
 
         output.append('G80\n')
         return output
-
 
 
 if __name__ == "__main__":
@@ -97,11 +119,11 @@ if __name__ == "__main__":
     drill.feed = 20
     drill.speed = 700
     drill.coolant = 'M7'
-    drill.z_clear_ = 1
+    drill.z_clear = 1
     drill.z_start = 0.02
     drill.z_end = -0.5
     drill.z_feed = 40
-    drill.bolt_hole_circle_xy(3, 5, (2.5, 2.5), 0)
+    drill.bolt_hole_circle_xy(10, 5, (2.5, 2.5), 0)
 
     def write_to_file(file_name, data, mode="w"):
         f = open(file_name, mode)
